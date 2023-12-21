f950f9a194e2e152b6f32a71cb8e718e614c4024
Johnathon.Deng <johnathon.deng@fjdynamics.com>
2023-12-21 15:38:22 +0800

    使用PCL显示配准后的点云对比

--- carto_map/include/carto_map/pointcloud_map_match.hpp
+++ carto_map/include/carto_map/pointcloud_map_match.hpp
@@ -1,5 +1,7 @@
 #include <pcl/point_cloud.h>
 #include <pcl/registration/icp.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/console/parse.h>
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
@@ -39,20 +41,20 @@ class PointcloudMapMatch {
       const pcl::PointCloud<pcl::PointXYZ>& source,
       const Eigen::Matrix4f& trans_matrix,
       const std::string& file_name) {
-    cv::Mat image(6000, 6000, CV_8UC3, cv::Scalar(255, 255, 255));
+    cv::Mat image(10000, 10000, CV_8UC3, cv::Scalar(255, 255, 255));
 
     pcl::PointCloud<pcl::PointXYZ> input_trans = input;
     pcl::transformPointCloud(input, input_trans, trans_matrix);
 
     for (const auto& point : input_trans.points) {
-      int x = static_cast<int>(point.x * 100 + image.cols / 2);
-      int y = static_cast<int>(point.y * 100 + image.rows / 2);
-      cv::circle(image, cv::Point(x, y), 1, cv::Scalar(0, 0, 255), -1);
+      int x = static_cast<int>(point.x * 1000.0 + image.cols / 2.0);
+      int y = static_cast<int>(point.y * 1000.0 + image.rows / 2.0);
+      cv::circle(image, cv::Point(x, y), 10, cv::Scalar(0, 0, 255), -1);
     }
     for (const auto& point : source.points) {
-      int x = static_cast<int>(point.x * 100 + image.cols / 2);
-      int y = static_cast<int>(point.y * 100 + image.rows / 2);
-      cv::circle(image, cv::Point(x, y), 1, cv::Scalar(255, 0, 0), -1);
+      int x = static_cast<int>(point.x * 1000.0 + image.cols / 2.0);
+      int y = static_cast<int>(point.y * 1000.0 + image.rows / 2.0);
+      cv::circle(image, cv::Point(x, y), 10, cv::Scalar(255, 0, 0), -1);
     }
 
     cv::imwrite(file_name, image);
@@ -134,54 +136,58 @@ void PointcloudMapMatch::MatchICP(
     const cartographer::mapping::Grid2D& source_grid,
     const cartographer::transform::Rigid2d& predict_pose,
     cartographer::transform::Rigid2d* pose_estimate) {
-  pcl::PointCloud<pcl::PointXYZ> cloud_input = GetPointcloudFromGrid(source_grid);
-
-  cartographer::transform::Rigid2d trans_pose({0.2,0.3}, 0.3);
-  Eigen::Matrix4f trans_matrix = FromRigid2d(trans_pose);
-  pcl::PointCloud<pcl::PointXYZ> cloud_trans = cloud_input;
-  pcl::transformPointCloud(cloud_input, cloud_trans, trans_matrix);
+  pcl::PointCloud<pcl::PointXYZ> cloud_input = GetPointcloudFromGrid(input_grid);
+  pcl::PointCloud<pcl::PointXYZ> cloud_source = GetPointcloudFromGrid(source_grid);
+  Eigen::Matrix4f predict_matrix = FromRigid2d(predict_pose);
+  // LOG(INFO) << "MatchICP predict_pose:" << predict_pose;
+  // cartographer::transform::Rigid2d trans_pose({0.2,0.3}, 0.3);
+  // cartographer::transform::Rigid2d trans_pose({-1.13986, 0.533425}, -1.82904);
+  // Eigen::Matrix4f predict_matrix = FromRigid2d(trans_pose);
+  pcl::PointCloud<pcl::PointXYZ> cloud_trans2 = cloud_input;
+  pcl::transformPointCloud(cloud_input, cloud_trans2, predict_matrix);
 
   pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-  icp.setMaximumIterations(100);
-  icp.setInputSource(cloud_trans.makeShared());
-  icp.setInputTarget(cloud_input.makeShared());
+  icp.setMaximumIterations(300);
+  icp.setInputSource(cloud_input.makeShared());
+  icp.setInputTarget(cloud_source.makeShared());
 
   pcl::PointCloud<pcl::PointXYZ> final_cloud;
-  icp.align(final_cloud);
+  icp.align(final_cloud, predict_matrix);
   LOG(INFO) << "MatchICP has converged:" << icp.hasConverged()
             << " score: " << icp.getFitnessScore();
+
   if (icp.hasConverged()) {
-    Eigen::Matrix4f final_matrix = icp.getFinalTransformation();
-    std::cout << "predict transformation " << std::endl << trans_matrix << std::endl;
-    std::cout << "final transformation " << std::endl << final_matrix << std::endl;
-    // SaveImageFromMatchPointcloud(cloud_input, cloud_trans, trans_matrix, "/home/dean/Pictures/submap1.png");
-    // SaveImageFromMatchPointcloud(cloud_input, cloud_trans, final_matrix, "/home/dean/Pictures/submap2.png");
+    Eigen::Matrix4f trans_matrix = icp.getFinalTransformation();
+    std::cout << "predict transformation " << std::endl << predict_matrix << std::endl;
+    std::cout << "final transformation " << std::endl << trans_matrix << std::endl;
+
+    pcl::PointCloud<pcl::PointXYZ> cloud_trans3 = cloud_input;
+    pcl::transformPointCloud(cloud_input, cloud_trans3, trans_matrix);
+    // 创建可视化对象
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer1(new pcl::visualization::PCLVisualizer("Point Cloud Viewer1"));
+    viewer1->setBackgroundColor(1.0, 1.0, 1.0);  // 设置背景颜色
+    // 添加点云数据
+    viewer1->addPointCloud<pcl::PointXYZ>(cloud_source.makeShared(), "cloud1");
+    viewer1->addPointCloud<pcl::PointXYZ>(cloud_trans2.makeShared(), "cloud2");
+    viewer1->addPointCloud<pcl::PointXYZ>(cloud_trans3.makeShared(), "cloud3");
+    // 设置点云颜色 原始点云红色  预测点云绿色  配准点云蓝色
+    viewer1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 0.0, 0.0, "cloud1");
+    viewer1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0.0, 1.0, 0.0, "cloud2");
+    viewer1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0.0, 0.0, 1.0, "cloud3");
+    // 设置点云大小
+    viewer1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5, "cloud1");
+    viewer1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5, "cloud2");
+    viewer1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5, "cloud3");
+    // 运行可视化窗口
+    while (!viewer1->wasStopped()) {
+        viewer1->spinOnce(100);
+        usleep(200);
+    }
+
+    // *pose_estimate = cartographer::transform::Project2D(ToRigid3d(trans_matrix));
+  } else {
+    // *pose_estimate = predict_pose;
   }
 
 
-
-  // pcl::PointCloud<pcl::PointXYZ> cloud_source =
-  //     GetPointcloudFromGrid(input_grid);
-  // Eigen::Matrix4f predict_matrix = FromRigid2d(predict_pose);
-
-  // pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-  // icp.setMaximumIterations(100);
-  // icp.setInputSource(cloud_source.makeShared());
-  // icp.setInputTarget(cloud_input.makeShared());
-
-  // pcl::PointCloud<pcl::PointXYZ> final_cloud;
-  // icp.align(final_cloud, predict_matrix);
-  // LOG(INFO) << "MatchICP has converged:" << icp.hasConverged()
-  //           << " score: " << icp.getFitnessScore();
-
-  // if (icp.hasConverged()) {
-  //   Eigen::Matrix4f trans_matrix = icp.getFinalTransformation();
-  //   std::cout << "predict transformation " << std::endl << predict_matrix << std::endl;
-  //   std::cout << "final transformation " << std::endl << trans_matrix << std::endl;
-  //   SaveImageFromMatchPointcloud(cloud_input, cloud_source, predict_matrix, "/home/dean/Pictures/submap1.png");
-  //   SaveImageFromMatchPointcloud(cloud_input, cloud_source, trans_matrix, "/home/dean/Pictures/submap2.png");
-  //   *pose_estimate = cartographer::transform::Project2D(ToRigid3d(trans_matrix));
-  // } else {
-  //   *pose_estimate = predict_pose;
-  // }
 }
